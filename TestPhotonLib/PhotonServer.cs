4485e28e88e4d1842eca1de17e0a6021037f5bff
afrokick <afrokick@mail.by>
2013-06-04 22:36:30 +0300

    Create base

---
+++ TestPhotonLib/PhotonServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ExitGames.Logging;
+using ExitGames.Logging.Log4Net;
+using Photon.SocketServer;
+using log4net.Config;
+
+namespace TestPhotonLib
+{
+    public class PhotonServer:ApplicationBase
+    {
+        private readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            Log.Debug("connect new client...");
+
+            return new UnityClient(initRequest.Protocol, initRequest.PhotonPeer);
+        }
+
+        protected override void Setup()
+        {
+            var file = new FileInfo(Path.Combine(BinaryPath, "log4net.config"));
+            if (file.Exists)
+            {
+                LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+                XmlConfigurator.ConfigureAndWatch(file);
+            }
+
+            Log.Debug("Server is setup!");
+        }
+
+        protected override void TearDown()
+        {
+            Log.Debug("Server is closed!");
+        }
+    }
+}
---
+++ TestPhotonLib/UnityClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExitGames.Logging;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace TestPhotonLib
+{
+    public class UnityClient:PeerBase
+    {
+        private readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
+        public UnityClient(IRpcProtocol protocol, IPhotonPeer unmanagedPeer) : base(protocol, unmanagedPeer)
+        {
+            Log.Debug("Client ip:" + unmanagedPeer.GetRemoteIP());
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            Log.Debug("Unknown operation!");
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            Log.Debug("Disconnected!");
+        }
+    }
+}
