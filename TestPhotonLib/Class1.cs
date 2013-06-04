1fd57c4b89ab5f37a0b640bba1dea047570f21d9
afrokick <afrokick@mail.by>
2013-06-04 21:22:18 +0300

    Create lib

---
+++ TestPhotonLib/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestPhotonLib
+{
+    public class Class1
+    {
+        public static void ShowMessage(string text)
+        {
+            System.Console.WriteLine("[LOG]" + text);
+        }
+    }
+}
