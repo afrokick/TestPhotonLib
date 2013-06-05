062171e50ecbbe7191a3bfc12e5cc48cc02dc4ba
afrokick <afrokick@mail.by>
2013-06-06 00:35:15 +0300

    Add Common Library

--- PhotoClient/Assets/_Scripts/PhotonServer.cs
+++ PhotoClient/Assets/_Scripts/PhotonServer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using ExitGames.Client.Photon;
+using TestPhotonLib.Common;
 using UnityEngine;
 
 public class PhotonServer : MonoBehaviour, IPhotonPeerListener
